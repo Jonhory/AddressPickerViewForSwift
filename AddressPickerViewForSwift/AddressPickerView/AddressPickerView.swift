c521806091663754b17ddaec20b9a002680b9dc7
jonhory <318075441@qq.com>
2017-03-22 11:32:50 +0800

    基本完成

--- AddressPickerViewForSwift/AddressPickerView/AddressPickerView.swift
+++ AddressPickerViewForSwift/AddressPickerView/AddressPickerView.swift
@@ -8,23 +8,156 @@
 
 import UIKit
 
+protocol AddressPickerViewDelegate: class {
+    func addressSure(province: String, city: String, region: String)
+    
+    func addressSure(provinceID: Int, cityID: Int, regionID: Int)
+}
 
 class AddressPickerView: UIView {
 
-    let SELFSIZE = UIScreen.main.bounds.size
-    
-    var dataDict: [String: Any]?
-    var provincesArr: [String]?
-    var citysDict: [String: Any]?
-    var regionsDict: [String: Any]?
-    
-    init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: SELFSIZE.width, height: 215))
-        loadAddressData()
+    class Province {
+        var name: String!
+        var id: Int!
+        var cityModelArr: [City] = []
     }
     
-    private override init(frame: CGRect) {
+    class City {
+        var name: String!
+        var id: Int!
+        var regionModelArr: [Region] = []
+    }
+    
+    class Region {
+        var name: String!
+        var id: Int!
+    }
+    
+    let SELFSIZE = UIScreen.main.bounds.size
+    let backView = UIButton()
+    var pickerView: UIPickerView?
+    var barView: UIView?
+    var cancelBtn: UIButton?
+    var sureBtn: UIButton?
+    var dateStr: String!
+    private let animateTime = 0.68
+    private let barHeight: CGFloat = 49.0
+    private let pickerHeight: CGFloat = 200
+    
+    weak var delegate:AddressPickerViewDelegate?
+    
+    /// 数据源初始化
+    var dataDict: [String: Any]?
+    /// 省字典
+    var provincesArr: [String]?
+    /// 省ID字典
+    var provinceIDDict: [String: Int]?
+    /// 城市字典
+    var citysDict: [String: Any]?
+    /// 城市ID字典
+    var cityIDDict: [String: Int]?
+    /// 地区字典
+    var regionsDict: [String: Any]?
+    /// 地区ID字典
+    var regionIDDict: [String: Int]?
+    /// 整体数据源
+    var provinceModelArr: [Province] = []
+    
+    func show() {
+        isHidden = false
+        
+        loadAddressPicker()
+        
+        UIView.animate(withDuration: animateTime) {
+            self.pickerView?.frame = CGRect(x: 0, y: self.bounds.height - self.pickerHeight, width: self.bounds.width, height: self.pickerHeight)
+            self.barView?.frame = CGRect(x: 0, y: self.bounds.height - self.pickerHeight - self.barHeight, width: self.bounds.width, height: self.barHeight)
+        }
+    }
+    
+    func hide() {
+        UIView.animate(withDuration: animateTime, animations: {
+            self.pickerView?.frame = CGRect(x: 0, y: self.bounds.height + self.barHeight, width: self.bounds.width, height: self.pickerHeight)
+            self.barView?.frame = CGRect(x: 0, y: self.bounds.height, width: self.bounds.width, height: self.barHeight)
+        }) { (finished) in
+            if finished {
+                self.isHidden = true
+            }
+        }
+    }
+    
+    func sure() {
+        if delegate != nil {
+            let selectP = pickerView!.selectedRow(inComponent: 0)
+            var selectC = pickerView!.selectedRow(inComponent: 1)
+            var selectR = pickerView!.selectedRow(inComponent: 2)
+            let p = provinceModelArr[selectP]
+            if selectC > p.cityModelArr.count - 1 {
+                selectC = p.cityModelArr.count - 1
+            }
+            let c = p.cityModelArr[selectC]
+            if selectR > c.regionModelArr.count - 1 {
+                selectR = c.regionModelArr.count - 1
+            }
+            let r = c.regionModelArr[selectR]
+            delegate?.addressSure(province: p.name, city: c.name, region: r.name)
+            delegate?.addressSure(provinceID: p.id, cityID: c.id, regionID: r.id)
+        }
+        hide()
+    }
+    
+    //MARK: 初始化
+    class func addTo(superView: UIView) -> AddressPickerView {
+        let pickerView = AddressPickerView(frame: CGRect(x: 0, y: 0, width: superView.frame.width, height: superView.frame.height))
+        superView.addSubview(pickerView)
+        return pickerView
+    }
+    
+    override init(frame: CGRect) {
         super.init(frame: frame)
+        isHidden = true
+        loadAddressData()
+        loadBackView()
+    }
+
+    private func loadBackView() {
+        addSubview(backView)
+        backView.frame = self.bounds
+        backView.addTarget(self, action: #selector(hide), for: .touchUpInside)
+        backView.backgroundColor = UIColor(red: 151/255.0, green: 255/255.0, blue: 255/255.0, alpha: 0.3)
+    }
+    
+    private func loadAddressPicker() {
+        if pickerView == nil {
+            let dframe = CGRect(x: 0, y: self.bounds.height + barHeight, width: self.bounds.width, height: pickerHeight)
+            
+            pickerView = UIPickerView(frame: dframe)
+            pickerView?.backgroundColor = UIColor(red: 239/255.0, green: 239/255.0, blue: 244/255.0, alpha: 1.0)
+            pickerView?.delegate = self
+            pickerView?.dataSource = self
+            addSubview(pickerView!)
+            
+            barView = UIView()
+            barView?.frame = CGRect(x: 0, y: self.bounds.height, width: self.bounds.width, height: barHeight)
+//            let backColor = UIColor(red: 240/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1.0)
+            barView?.backgroundColor = UIColor.white
+            addSubview(barView!)
+            
+            cancelBtn = UIButton(type: .custom)
+            cancelBtn?.setTitle("取消", for: UIControlState())
+            cancelBtn?.addTarget(self, action: #selector(hide), for: .touchUpInside)
+            let cancelColor = UIColor(red: 102/255.0, green: 102/255.0, blue: 102/255.0, alpha: 1.0)
+            cancelBtn?.setTitleColor(cancelColor, for: UIControlState())
+            cancelBtn?.frame = CGRect(x: 0, y: 0, width: 80, height: barHeight)
+            barView?.addSubview(cancelBtn!)
+            
+            sureBtn = UIButton(type: .custom)
+            sureBtn?.setTitle("确定", for: UIControlState())
+            sureBtn?.addTarget(self, action: #selector(sure), for: .touchUpInside)
+            let sureColor = UIColor(red: 5/255.0, green: 5/255.0, blue: 5/255.0, alpha: 1.0)
+            sureBtn?.setTitleColor(sureColor, for: UIControlState())
+            sureBtn?.frame = CGRect(x: (barView?.frame.size.width)! - 80, y: 0, width: 80, height: barHeight)
+            barView?.addSubview(sureBtn!)
+        }
     }
     
     //MARK: - 加载数据源
@@ -48,7 +181,42 @@ class AddressPickerView: UIView {
         citysDict = dataDict!["city"] as! [String : Any]?
         regionsDict = dataDict!["region"] as! [String : Any]?
         
-        print(provincesArr)
+        if provincesArr == nil || citysDict == nil  || regionsDict == nil { return }
+        
+        provinceIDDict = dataDict!["provinceID"] as! [String: Int]?
+        cityIDDict = dataDict!["cityID"] as! [String: Int]?
+        regionIDDict = dataDict!["regionID"] as! [String: Int]?
+        
+        if provinceIDDict == nil || cityIDDict == nil || regionIDDict == nil { return }
+
+        let provinceCount = provincesArr!.count
+        for i in 0..<provinceCount {
+            let pName = provincesArr![i]
+            let citys = citysDict![pName] as! [String]
+            let p = Province()
+            p.name = pName
+            p.id = provinceIDDict![pName]
+            
+            var cityModels: [City] = []
+            for cityName in citys {
+                let regionArr = regionsDict![cityName] as! [String]
+                let cityModel = City()
+                cityModel.id = cityIDDict![cityName]
+                cityModel.name = cityName
+                
+                var regionModels: [Region] = []
+                for regionName in regionArr {
+                    let regionModel = Region()
+                    regionModel.name = regionName
+                    regionModel.id = regionIDDict![regionName]
+                    regionModels.append(regionModel)
+                }
+                cityModel.regionModelArr = regionModels
+                cityModels.append(cityModel)
+            }
+            p.cityModelArr = cityModels
+            provinceModelArr.append(p)
+        }
     }
     
     private func dictionaryWith(jsonString: String?) -> [String: Any]? {
@@ -71,4 +239,93 @@ class AddressPickerView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+}
+
+extension AddressPickerView: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return provinceModelArr[row].name
+        case 1:
+            let selectP = pickerView.selectedRow(inComponent: 0)
+            let p = provinceModelArr[selectP]
+            if row > p.cityModelArr.count - 1 {
+                return nil
+            }
+            return p.cityModelArr[row].name
+        case 2:
+            let selectP = pickerView.selectedRow(inComponent: 0)
+            let selectC = pickerView.selectedRow(inComponent: 1)
+            let p = provinceModelArr[selectP]
+            if selectC > p.cityModelArr.count - 1 {
+                return nil
+            }
+            let c = p.cityModelArr[selectC]
+            if row > c.regionModelArr.count - 1 {
+                return nil
+            }
+            return c.regionModelArr[row].name
+        default:
+            return nil
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            let selectC = pickerView.selectedRow(inComponent: 1)
+            let selectR = pickerView.selectedRow(inComponent: 2)
+            pickerView.reloadComponent(1)
+            pickerView.selectRow(selectC, inComponent: 1, animated: true)
+            pickerView.reloadComponent(2)
+            pickerView.selectRow(selectR, inComponent: 2, animated: true)
+            break
+        case 1:
+            let selectR = pickerView.selectedRow(inComponent: 2)
+            pickerView.reloadComponent(2)
+            pickerView.selectRow(selectR, inComponent: 2, animated: true)
+            break
+        default:
+            break
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var label = view as? UILabel
+        if label == nil {
+            label = UILabel()
+            label?.textColor = UIColor(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1.0)
+            label?.adjustsFontSizeToFitWidth = true
+            label?.textAlignment = .center
+            label?.font = UIFont.systemFont(ofSize: 15)
+        }
+        label?.text = self.pickerView(pickerView, titleForRow: row, forComponent: component)
+        return label!
+    }
+}
+
+extension AddressPickerView: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return provinceModelArr.count
+        case 1:
+            let selectP = pickerView.selectedRow(inComponent: 0)
+            return provinceModelArr[selectP].cityModelArr.count
+        case 2:
+            let selectP = pickerView.selectedRow(inComponent: 0)
+            let selectC = pickerView.selectedRow(inComponent: 1)
+            let p = provinceModelArr[selectP]
+            if selectC > p.cityModelArr.count - 1 {
+                return 0
+            }
+            return p.cityModelArr[selectC].regionModelArr.count
+        default:
+            return 0
+        }
+    }
 }
--- AddressPickerViewForSwift/ViewController.swift
+++ AddressPickerViewForSwift/ViewController.swift
@@ -10,21 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var picker: AddressPickerView?
+    let showIDLabel = UILabel()
+    let showStrLabel = UILabel()
+    let btn = UIButton(type: .custom)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        showIDLabel.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        showIDLabel.textColor = UIColor.red
+        showIDLabel.text = "这里展示选中的对应的ID"
+        showIDLabel.textAlignment = .center
+        showIDLabel.center = CGPoint(x: view.center.x, y: view.center.y - 100)
+        view.addSubview(showIDLabel)
+        
+        showStrLabel.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        showStrLabel.textColor = UIColor.blue
+        showStrLabel.textAlignment = .center
+        showStrLabel.text = "这里展示选中的字符串"
+        showStrLabel.center = CGPoint(x: view.center.x, y: view.center.y - 150)
+        view.addSubview(showStrLabel)
         
         
-
-        let picker = AddressPickerView()
-
-        picker.backgroundColor = UIColor.lightGray
-        view.addSubview(picker)
-        
-        
+        picker = AddressPickerView.addTo(superView: view)
+        picker?.delegate = self
+        picker?.show()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if picker!.isHidden {
+            picker!.show()
+        } else {
+            picker!.hide()
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,3 +55,12 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: AddressPickerViewDelegate {
+    func addressSure(provinceID: Int, cityID: Int, regionID: Int) {
+        showIDLabel.text = "\(provinceID) , \(cityID) , \(regionID)"
+    }
+    
+    func addressSure(province: String, city: String, region: String) {
+        showStrLabel.text = province + city + region
+    }
+}
